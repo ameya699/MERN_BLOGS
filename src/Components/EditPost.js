ae01a42d4f983840eb90390d15fcb42bd5367019
ameya699 <awatadeameya68@gmail.com>
2024-01-29 11:25:55 +0530

    completed the frontend

--- src/Components/EditPost.js
+++ src/Components/EditPost.js
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css'
 const EditPost = () => {
+  const [title,setTitle]=useState('');
+  const [category,setCategory]=useState('Uncategorized');
+  const [description,setDescription]=useState('');
+  const [thumbnail,setThumbnail]=useState('');
+  const POST_CATEGORIES=["Agriculture","Business","Education","Entertainment","Art","Investment","uncategorized","Weather"]
+  const formats=[
+    'header',
+    'bold','italic','underline','strike','blockquote',
+    'list','bullet','indent','link','image'
+  ]
+  const module={
+    toolbar:[
+      [{'header':[1,2,false]}],
+      ['bold','italic','underline','strike','blockquote'],
+      [{'list':'ordered'},{'list':'bullet'},{'indent':'-1'},{'indent':'+1'}],
+      ['link','image']
+      ['clean']
+    ]
+  }
   return (
-    <div>EditPost</div>
+    <section className="create-post">
+      <div className="container">
+        <h2>Edit Post</h2>
+        <p className="form__error-message">This is an error Message</p>
+      <form className="form create-post__form">
+        <input type='text' placeholder='Title' value={title} onChange={(e=>setTitle(e.target.value))}/>
+        <select name='category' value={category} onChange={(e)=>setCategory(e.target.value)}>
+          {
+            POST_CATEGORIES.map((cat=><option key={cat}>{cat}</option>))
+          }
+         </select>
+         <ReactQuill modules={module}  formats={formats} value={description} onChange={(e)=>setDescription(e.target.value)}/>
+         <input type="file" onChange={(e)=>setThumbnail(e.target.files[0])} accept='png, jpeg,jpg'/>
+          <button type='btn primary' typeof='submit'>Update</button>
+       </form>
+      </div>
+    </section>
   )
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from "./assets/logo.png"
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [isNavShowing,setIsNavShowing]=useState(window.innerWidth>800?true:false);
+  const closeHandler=()=>{
+    if(window.innerWidth<800){
+      setIsNavShowing(false);
+    }
+    else{
+      setIsNavShowing(true);
+    }
+  }
   return (
     <nav>
       <div className='container nav__container'>
     <Link to="/" className='nav__logo'>
-    <img src={Logo} alt="Navbar Logo"/>
+    <img src={Logo} alt="Navbar Logo" onClick={closeHandler}/>
     </Link>
-    <ul className='nav__menu'>
-      <li><Link to="/profile/sfdgrf">Ameya Awatade</Link></li>
-      <li><Link to="/create">Create Post</Link></li>
-      <li><Link to="/authors">Authors</Link></li>
-      <li><Link to="/logout">Logout</Link></li>
-    </ul>
-    <button className='nav__toggle-btn'>
-    <AiOutlineClose/>
+    {isNavShowing && <ul className='nav__menu'>
+      <li><Link to="/profile/sfdgrf" onClick={closeHandler}>Ameya Awatade</Link></li>
+      <li><Link to="/create" onClick={closeHandler}>Create Post</Link></li>
+      <li><Link to="/authors" onClick={closeHandler}>Authors</Link></li>
+      <li><Link to="/logout" onClick={closeHandler}>Logout</Link></li>
+    </ul>}
+    <button className='nav__toggle-btn' onClick={()=>setIsNavShowing(!isNavShowing)}>
+    {isNavShowing?<AiOutlineClose/>:<FaBars/>}
     </button>
       </div>
     </nav>
