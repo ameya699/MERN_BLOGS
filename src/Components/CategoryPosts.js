f0e860a312d482001ad27338801b410cebdac5c3
ameya699 <awatadeameya68@gmail.com>
2024-02-06 10:40:40 +0530

    implemented category posts

--- src/Components/CategoryPosts.js
+++ src/Components/CategoryPosts.js
@@ -1,15 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {DUMMY_POSTS} from "../data"
 import PostItem from "./PostItem"
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const CategoryPosts = () => {
 
-  const [posts,setPosts]=useState(DUMMY_POSTS);
+
+  const [posts,setPosts]=useState([]);
+  const [loading,setLoading]=useState(false);
+  const {category}=useParams();
+
+  useEffect(()=>{
+    const fetchPosts=async()=>{
+      setLoading(true);
+    try
+    { const response =await axios.get(`${process.env.REACT_APP_BASE_URL}/posts/categories/${category}`);
+    const authorposts=await response?.data;
+    setPosts(authorposts);
+  }
+  catch(err){
+    console.log(err);
+  }
+  setLoading(false);
+
+    }
+    fetchPosts();
+  },[])
   return (
-    <section>
+    <section className='posts'>
         {posts.length>0 ? <div className='container posts__container'>
         {
-            posts.map(({id,thumbnail,category,title,desc,authorID})=><PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID}/>)
+            posts.map(({id,thumbnail,category,title,desc,authorID,createdAt})=><PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} desc={desc} authorID={authorID} createdAt={createdAt}/>)
         }
         </div>
       :<h2 className='center'>No Posts Found</h2>}
